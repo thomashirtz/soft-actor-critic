7ac2970537686138220d73864fb250870212ae90
Thomas <hirtzt@gmail.com>
2021-05-05 14:47:34 +0200

    Update tests

--- tests/test_train.py
+++ tests/test_train.py
@@ -3,7 +3,4 @@ import soft_actor_critic
 
 if __name__ == '__main__':
     environment_id = 'LunarLanderContinuous-v2'
-    name = 'name_of_a_previous_run'
-    soft_actor_critic.evaluate(environment_id, run_name=name)
-
-
+    soft_actor_critic.train(environment_id)
